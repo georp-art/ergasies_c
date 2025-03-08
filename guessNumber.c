888d06051eeb8989469b9af40c9888e8914eacd4
georp-art <georpappa03@gmail.com>
2025-03-08 15:40:42 +0200

    Update and rename atomiki1.c to guessNumber.c

--- guessNumber.c
+++ guessNumber.c
@@ -1,59 +1,64 @@
 #include<stdio.h>
 
+/* Dhmioyrgia enos paixnidiou me 2 paiktes. 
+O prwtos paikths prepei na plhktrologhsei enan arithmo apo to 1 ews to 1000. 
+O deyteros paikths prepei na mantepsei ayton ton aritmo.
+Otan h mantepsia einai swsth to programma tha emfanizei kai tis prospathies pou ekane */
+
 int main(void)
 
 {
-	// eisodos tou programmatos//
+// eisodos tou programmatos//
 	
-int x,i,k,fores;          
+int number,i,guess,count;          
 char a,b;
 
  //o xrhsths A pliktrologei enan ari8mo apo to 1 ews to 1000//
  
  printf("paixti A dose enan ari8mo sth klimaka [1 ews 1000]:");
- scanf("%d",&x);
+ scanf("%d",&number);
  
 //elegxos tou ari8mou pou plhktologisame// 
- while (x<1 || x>1000)
+ while (number<1 || number>1000)
   {
   	printf ("/n paixth A dose pali enan ari8mo sth klimaka [1 ews 1000]:");
-  	scanf("%d",&x);
+  	scanf("%d",&number);
   }
 
 for (i=1; i<150; i++)
   {
   	printf("\n");
    } 
-  fores=1;
+  count=1;
   
   //o B paixths prepei na mantepsei poios ari8mos eiani//
   
   printf(" paixti B hr8e h wra na mantepseis poios einai o ari8mos autos:");
-  scanf("%d",&k);
+  scanf("%d",&guess);
   
   //elegxos tou ari8mou an einai megakyteros h mikroteros apo auton pou o A paixths dhkwse//
   
-  while (k<x || k>x)
+  while (guess !=number)
    {
-    if (k<x)
+    if (guess<number)
      {
 	 printf("o ari8mos pou 8elw na brw einai megalyteros");
 	 printf("\n dose pali enan allo ari8mo:");
-	 scanf("%d",&k);
+	 scanf("%d",&guess);
 	 }
-	else if (k>x)
+	else if (guess>number)
 	 {
 	 printf("o ari8mos pou 8elw na brw einai mikroteros");
 	 printf("\n dose pali enan allo ari8mo:");
-	 scanf("%d",&k);
+	 scanf("%d",&guess);
 	 }
-	 fores++;
+	 count++;
    }
    
    //emfanish tou ari8mou kai oi fores pou xreiasrthkan //
    
-   printf("\n o ari8mos bre8hke kai einai o: %d" ,k);
-   printf("\n o ari8mos bre8hke meta apo prospa8eies:%d " ,fores);
+   printf("\n o ari8mos bre8hke kai einai o: %d" ,guess);
+   printf("\n o ari8mos bre8hke meta apo prospa8eies:%d " ,count);
    
    printf("\n");
    
